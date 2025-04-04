d4331c8bef62d07bb9a0318cd97186cd3088614c
sourav das <sd1535060@gmail.com>
2025-04-04 14:43:10 +0530

    updated attendance page, student_reg pg, export pg
    updated attendance page, student_reg pg, export pg with header and logo . also added more styling but js part kept as it is.

--- Student_Reg.js
+++ Student_Reg.js
@@ -2,5 +2,4 @@ document.getElementById('studentForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Form Submitted Successfully!');
     alert('You can now Log in');
-    
 });
